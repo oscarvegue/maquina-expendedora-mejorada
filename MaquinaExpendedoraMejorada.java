3e9dabaca63d29958de2cbc5789780f2ea3aed2d
Oscar <oscarfuersuarez27@gmail.com>
2022-12-02 12:18:25 +0100

    Vaciar maquina

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -10,8 +10,7 @@ public class MaquinaExpendedoraMejorada {
     private String estacionOrigen;
     // El destino del billete
     private String estacionDestino;
-    // Se vacía todo el dinero de la máquina
-    private int vaciarDineroDeLaMaquina;
+
     /**
      * Crea una maquina expendedora de billetes de tren con el 
      * precio del billete y el origen y destino dados. Se asume que el precio
@@ -40,11 +39,11 @@ public class MaquinaExpendedoraMejorada {
      * Vacía todo el dinero de la máquina
      */
     public int getVaciarDineroDeLaMaquina() {
-        int Conjuntodepositos;
-        Conjuntodepositos = totalDineroAcumulado + balanceClienteActual;
+        int conjuntodepositos;
+        conjuntodepositos = totalDineroAcumulado + balanceClienteActual;
         balanceClienteActual = 0;
         totalDineroAcumulado = 0;
-        return Conjuntodepositos;
+        return conjuntodepositos;
     }
     
     /**
